f1bf5f31de4941eb0873bc9dd37bc582ae876e04
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-07-02 23:53:45 -0400

    add RRSS in credits

--- controller/template.controller.js
+++ controller/template.controller.js
@@ -8,8 +8,8 @@ export const createCredits = () => {
     <h2 class="credits-content__title">Desarrollado por A. Barrera</h2>
     <p class="credits-content__subtitle">Oracle Next Education G3 - 2022</p>
     <div class="credits-content__network">
-      <a href="#"><img class="credits-content__icon" src="assets/images/social/github.svg" alt="Icono github"></a>
-      <a href="#"><img class="credits-content__icon" src="assets/images/social/linkedIn.svg" alt="Icono linkedIn"></a>
+      <a target="_blank" href="https://github.com/AlejandroBH"><img class="credits-content__icon" src="assets/images/social/github.svg" alt="Icono github"></a>
+      <a target="_blank" href="https://www.linkedin.com/in/alejandro-bh91/"><img class="credits-content__icon" src="assets/images/social/linkedIn.svg" alt="Icono linkedIn"></a>
     </div>
     <div class="credits-content__buttons">
       <button class="button" data-button-close>Aceptar</button>
